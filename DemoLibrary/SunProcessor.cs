99226d64a0ec3f7b90bbfb792b09ebd9ee1d1c82
Luis Magalhaes <sprintlmaga@gmail.com>
2020-02-27 15:25:13 +0000

    Add project files.

---
+++ DemoLibrary/SunProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace DemoLibrary
+{
+    public class SunProcessor
+    {
+
+        public static async Task<SunModel> LoadSunInformation()
+        {
+            string url = "http://api.sunrise-sunset.org/json?lat=52.18935&lng=-2.22001&date=today";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    SunResultModel result = await response.Content.ReadAsAsync<SunResultModel>();
+
+                    return result.Results;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
